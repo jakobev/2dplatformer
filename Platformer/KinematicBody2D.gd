1e9e4fd87bafea5efa661036609123452175277b
jakobev <jakobev1@gmail.com>
2020-03-04 21:31:06 +0100

    updated script and tileset

--- Platformer/KinematicBody2D.gd
+++ Platformer/KinematicBody2D.gd
@@ -12,23 +12,25 @@ func _physics_process(delta):
 	motion.y += gravity
 	
 	if Input.is_action_pressed("ui_right"):
-		get_node("AnimatedSprite").play("run")
 		motion.x = speed
-	elif Input.is_action_pressed("ui_left"):
-		get_node("AnimatedSprite").flip_h
+		get_node("AnimatedSprite").flip_h = false
 		get_node("AnimatedSprite").play("run")
+	elif Input.is_action_pressed("ui_left"):
 		motion.x = -speed
+		get_node("AnimatedSprite").flip_h = true
+		get_node("AnimatedSprite").play("run")
 	else:
 		get_node("AnimatedSprite").play("idle")
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			get_node("AnimatedSprite").play("jump")
 			motion.y = -jump
-		else:
-			motion.y = 0
+	else:
+		#print("ist in luft")
+		get_node("AnimatedSprite").play("jump")
 		
+	
 		
 	motion = move_and_slide(motion, up)
 	pass
