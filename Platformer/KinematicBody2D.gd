9d0f87794f4518aefa459f001f6e2805325f5ecd
jakobev <jakobev1@gmail.com>
2020-03-05 17:02:17 +0100

    set up player as scene and added second test world with boss

--- Platformer/KinematicBody2D.gd
+++ Platformer/KinematicBody2D.gd
@@ -2,33 +2,46 @@ extends KinematicBody2D
 
 
 var motion = Vector2()
-var speed = 200
+var acc = 50
 var gravity = 10
 var jump = 400
 var up = Vector2(0, -1)
+var test = 10
+var max_speed = 200
 
 
 func _physics_process(delta):
+	var friction = false
 	motion.y += gravity
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = speed
+		motion.x = min(motion.x + acc, max_speed)
 		get_node("AnimatedSprite").flip_h = false
 		get_node("AnimatedSprite").play("run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -speed
+		motion.x = max(motion.x - acc, -max_speed)
 		get_node("AnimatedSprite").flip_h = true
 		get_node("AnimatedSprite").play("run")
 	else:
+		print("Im idle zustand")
 		get_node("AnimatedSprite").play("idle")
-		motion.x = 0
+		friction = true
+		
 	
 	if is_on_floor():
+		print("is on floor")
 		if Input.is_action_pressed("ui_up"):
 			motion.y = -jump
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		#print("ist in luft")
-		get_node("AnimatedSprite").play("jump")
+		print("ist in luft ?!?!?!")
+		if motion.y < 0:
+			get_node("AnimatedSprite").play("jump")
+		else:
+			get_node("AnimatedSprite").play("fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 		
 	
 		
