fec55c9fcb5db6af35275bd6b8382e76c033391c
jakobev <jakobev1@gmail.com>
2020-03-12 11:37:51 +0100

    ready with autotile

--- Platformer/WorldComplete.gd
+++ Platformer/WorldComplete.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
+export(String, FILE, "*.tscn") var world_scene
 
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Ninja":
-			get_tree().change_scene("World2.tscn")
+			get_tree().change_scene(world_scene)
